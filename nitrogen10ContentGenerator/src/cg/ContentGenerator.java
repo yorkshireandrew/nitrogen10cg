0a454104113451e6e94b559c4118c6ae224a2ee9
andrew <andrew@LAPTOP>
2012-12-19 17:25:39 +0000

    added basic views buttons

--- nitrogen10ContentGenerator/src/cg/ContentGenerator.java
+++ nitrogen10ContentGenerator/src/cg/ContentGenerator.java
@@ -11,16 +11,15 @@ import java.io.*;
 public class ContentGenerator extends JFrame{
 	
 	/** buttons for selecting the view */
-	/*
-	final FixedSizeButton frontViewButton;
-	final FixedSizeButton leftViewButton;
-	final FixedSizeButton backViewButton;
-	final FixedSizeButton rightViewButton;
-	final FixedSizeButton topViewButton;
-	final FixedSizeButton bottomViewButton;
-	final FixedSizeButton greySpacer;
-	final ButtonGroup viewButtonGroup;
-	*/
+	
+	FixedSizeIconToggleButton frontViewButton;
+	FixedSizeIconToggleButton leftViewButton;
+	FixedSizeIconToggleButton backViewButton;
+	FixedSizeIconToggleButton rightViewButton;
+	FixedSizeIconToggleButton topViewButton;
+	FixedSizeIconToggleButton bottomViewButton;
+	ButtonGroup viewButtonGroup;
+	
 	
 	/** buttons for picking vertexes */
 	/*
@@ -94,6 +93,7 @@ public class ContentGenerator extends JFrame{
 		Box testBox = new Box(BoxLayout.Y_AXIS);
 		testBox.add(test1);
 		testBox.add(test2);
+		createViewButtons(testBox);
 		getContentPane().add(testBox);
 		getContentPane().validate();
 		
@@ -105,6 +105,53 @@ public class ContentGenerator extends JFrame{
 	public static void main(String[] args) {
 		ContentGenerator myContentGenerator = new ContentGenerator();
 		myContentGenerator.setVisible(true);
+	}
+	
+	void createViewButtons(Container container)
+	{
+		Box topBox = new Box(BoxLayout.X_AXIS);
+		Box midBox = new Box(BoxLayout.X_AXIS);
+		Box bottomBox = new Box(BoxLayout.X_AXIS);
+		
+		// create top box
+		topBox.add(new FixedSizeButton("/res/greySpacer.PNG"));
+		topViewButton = new FixedSizeIconToggleButton(this,"/res/topViewButton.PNG","/res/topViewButtonSelected.PNG");
+		topBox.add(topViewButton);
+		topBox.add(new FixedSizeButton("/res/greySpacer.PNG"));
+		topBox.add(new FixedSizeButton("/res/greySpacer.PNG"));
+
+		// create mid box
+		frontViewButton = new FixedSizeIconToggleButton(this,"/res/frontViewButton.PNG","/res/frontViewButtonSelected.PNG");
+		midBox.add(frontViewButton);
+		leftViewButton = new FixedSizeIconToggleButton(this,"/res/leftViewButton.PNG","/res/leftViewButtonSelected.PNG");
+		midBox.add(leftViewButton);
+		backViewButton = new FixedSizeIconToggleButton(this,"/res/backViewButton.PNG","/res/backViewButtonSelected.PNG");
+		midBox.add(backViewButton);
+		rightViewButton = new FixedSizeIconToggleButton(this,"/res/rightViewButton.PNG","/res/rightViewButtonSelected.PNG");
+		midBox.add(rightViewButton);
+		
+		// create bottom box
+		bottomBox.add(new FixedSizeButton("/res/greySpacer.PNG"));
+		bottomViewButton = new FixedSizeIconToggleButton(this,"/res/bottomViewButton.PNG","/res/bottomViewButtonSelected.PNG");
+		bottomBox.add(bottomViewButton);
+		bottomBox.add(new FixedSizeButton("/res/greySpacer.PNG"));
+		bottomBox.add(new FixedSizeButton("/res/greySpacer.PNG"));
+		
+		viewButtonGroup = new ButtonGroup();
+		viewButtonGroup.add(topViewButton);
+		viewButtonGroup.add(frontViewButton);
+		viewButtonGroup.add(leftViewButton);
+		viewButtonGroup.add(backViewButton);
+		viewButtonGroup.add(rightViewButton);
+		viewButtonGroup.add(bottomViewButton);
+		frontViewButton.setSelected(true);
+		
+		Box outerBox = new Box(BoxLayout.Y_AXIS);
+		outerBox.add(topBox);
+		outerBox.add(midBox);
+		outerBox.add(bottomBox);
+		container.add(outerBox);
+		
 	}
 
 }
